c235592caaf09430bb80a01941360db37927740d
rafbac <r.baczynski.sielaczek@gmail.com>
2024-04-12 15:40:06 +0200

    new classes: CartPage, ProductPage conversion to POM

--- src/test/java/CartTests.java
+++ src/test/java/CartTests.java
@@ -1,3 +1,4 @@
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,17 +14,20 @@ public class CartTests {
     WebDriverWait wait;
     String baseURL = "https://fakestore.testelka.pl/";
 
+
     By addToCartFromProductButtonLocator = By.cssSelector("[name=add-to-cart]");
     By goToCartFromProductButtonLocator = By.cssSelector(".woocommerce-message>.button");
-    By productIsInCartLocator = By.cssSelector("tr.cart_item");
     By updateCartButtonLocator = By.cssSelector("[name=update_cart]");
     By loadingIconLocator = By.cssSelector(".blockUI");
     By quantityFieldInCartLocator = By.cssSelector("input.qty");
     By totalPriceInCartLocator = By.cssSelector(".cart-subtotal [data-title=Kwota]");
     By emptyCartInfoLocator = By.cssSelector(".entry-content .cart-empty");
+    By productIsInCartLocator = By.cssSelector("tr.cart_item");
 
     String productWindSurURL = "/product/fuerteventura-sotavento/";
+    String productWindSurURLSlug = "/fuerteventura-sotavento/";
     String productWspinFerURL = "/product/wspinaczka-via-ferraty/";
+    String productWspinFerURLSlug = "/wspinaczka-via-ferraty/";
 
     @BeforeEach
     public void setup(){
@@ -35,34 +39,45 @@ public class CartTests {
         driver.quit();
     }
     @Test
-    @DisplayName("No product added to cart should art be empty")
+    @DisplayName("No product added to cart should cart be emptyPOM")
     public void emptyCart(){
+        CartPage cartpage = new CartPage(driver);
+        cartpage.go();
 
-        //driver.get(baseURL + productWindSurURL);
-        //driver.findElement(addToCartFromProductButtonLocator).click();
-
-        driver.get(baseURL + "/koszyk/");
-
-        Assertions.assertEquals(0, driver.findElements(productIsInCartLocator).size(),
+        Assertions.assertEquals(0, cartpage.getNumberOfProducts(),
                 "Number of products in cart is not 0");
-    }
 
+    }
     @Test
-    @DisplayName("No product added to cart should displayed info about empty cart")
+    @DisplayName("Two product added to cart and deleted should displayed info about empty cart")
     public void emptyCartInfo(){
-
-    }
-
-
-    @Test
-    @DisplayName("Product added to cart should cart have one product")
-    public void oneProductCart(){
         driver.get(baseURL + productWspinFerURL);
         driver.findElement(addToCartFromProductButtonLocator).click();
 
-        driver.get(baseURL + "/koszyk/");
+        driver.get(baseURL + productWindSurURL);
+        driver.findElement(addToCartFromProductButtonLocator).click();
 
-        int numberOfProducts= driver.findElements(productIsInCartLocator).size();
+        driver.findElement(goToCartFromProductButtonLocator).click();
+        driver.findElement(By.cssSelector(".woocommerce-cart-form__cart-item .remove")).click();
+
+        wait.until(ExpectedConditions.numberOfElementsToBe(loadingIconLocator, 0));
+
+        driver.findElement(By.cssSelector(".woocommerce-cart-form__cart-item .remove")).click();
+
+        wait.until(ExpectedConditions.numberOfElementsToBe(loadingIconLocator, 0));
+
+        Assertions.assertEquals("Twój koszyk jest pusty.", driver.findElement(emptyCartInfoLocator).getText(),
+                "The cart is not empty");
+
+    }
+    @Test
+    @DisplayName("Product added to cart should cart have one productPOM")
+    public void oneProductCart(){
+        ProductPage productPage = new ProductPage(driver);
+        CartPage cartPage = productPage.go(productWindSurURLSlug).addToCart().goToCart();
+
+        int numberOfProducts= cartPage.getNumberOfProducts();
+
         Assertions.assertEquals(1, numberOfProducts,
                 "Expected number of products in cart: 1" +
                 "\n Actual number of products: " + numberOfProducts);
@@ -96,7 +111,6 @@ public class CartTests {
         Assertions.assertEquals("5 598,00 zł", driver.findElement(totalPriceInCartLocator).getText(),
                 "Total price after quantity update is not what we expected");
     }
-
     @Test
     @DisplayName("Changing quantity in cart to negative should not update total price")
     public void changingQuantityNegativeAndPrice(){
