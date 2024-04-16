ed8beb4896d4717c478c7359bcd3d5610d90ede1
rafbac <r.baczynski.sielaczek@gmail.com>
2024-04-16 22:55:51 +0200

    StoreHeaderComponent: composition

--- src/test/java/pageobjects/BasePage.java
+++ src/test/java/pageobjects/BasePage.java
@@ -1,9 +1,23 @@
 package pageobjects;
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public abstract class BasePage {
+    private final By loadingIcon = By.cssSelector(".blockUI");
     protected final WebDriver driver;
     protected BasePage (WebDriver driver){ this.driver = driver; }
+
+
+    protected void waitForLoadingIcons(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.numberOfElementsToBe(loadingIcon, 0));
+    }
+
     protected final String baseURL = "https://fakestore.testelka.pl/";
+
+
 }
--- src/test/java/tests/CartTests.java
+++ src/test/java/tests/CartTests.java
@@ -1,9 +1,7 @@
 package tests;
-
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import pageobjects.CartPage;
 import pageobjects.ProductPage;
 
