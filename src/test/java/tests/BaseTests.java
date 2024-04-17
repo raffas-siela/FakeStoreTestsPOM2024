a8071be33901d60f13226aeb2ba8148e8eeede67
rafbac <r.baczynski.sielaczek@gmail.com>
2024-04-17 15:15:09 +0200

    configuration.properties + ConfigurationReader

--- src/test/java/tests/BaseTests.java
+++ src/test/java/tests/BaseTests.java
@@ -1,14 +1,29 @@
 package tests;
+import helpers.ConfigurationReader;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseTests {
-    WebDriver driver;
+    protected WebDriver driver;
+    private static ConfigurationReader configuration;
+    @BeforeAll
+    public static void loadConfiguration(){
+        configuration = new ConfigurationReader();
+    }
     @BeforeEach
     public void setup(){
-        driver = new ChromeDriver();
+        String browser = configuration.getBrowser();
+        boolean isHeadless = configuration.isHeadless();
+        switch (browser) {
+            case "firefox" -> driver = new FirefoxDriver();
+            case "chrome" -> driver = new ChromeDriver();
+            case "edge" -> driver = new EdgeDriver();
+        }
     }
     @AfterEach
     public void quitDriver(){
