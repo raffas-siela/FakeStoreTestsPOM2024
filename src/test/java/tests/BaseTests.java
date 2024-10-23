d438054b5372501b57b354f5c2412f66547a3ac2
rafbac <r.baczynski.sielaczek@gmail.com>
2024-10-23 15:58:05 +0200

    cleaning

--- src/test/java/tests/BaseTests.java
+++ src/test/java/tests/BaseTests.java
@@ -6,10 +6,21 @@ import helpers.NoSuchBrowserException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import pageobjects.CartPage;
+import pageobjects.ProductPage;
 
 public class BaseTests {
     protected Browser browser;
     private static ConfigurationReader configuration;
+
+    public CartPage addProductToCard (String productUrl){
+        ProductPage productPage = new ProductPage(browser);
+        return productPage
+                .go(productUrl)
+                .closeInfoButton()
+                .addToCart()
+                .goToCart();
+    }
 
     @BeforeAll
     public static void loadConfiguration(){
@@ -28,5 +39,10 @@ public class BaseTests {
     @AfterEach
     public void quitDriver(){
         browser.driver.quit();
+    }
+    protected double parsePrice(String price) {
+        // Usunięcie spacji, symbolu waluty i zamiana przecinka na kropkę
+        String cleanedPrice = price.replace(" ", "").replace("zł", "").replace(",", ".");
+        return Double.parseDouble(cleanedPrice);
     }
 }
