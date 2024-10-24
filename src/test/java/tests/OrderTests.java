26a61418146adfe05e6a7b455c55bc74891ba513
rafbac <r.baczynski.sielaczek@gmail.com>
2024-10-24 12:47:12 +0200

    new test OrderTests: - "order_without_account_creation"

--- src/test/java/tests/OrderTests.java
+++ src/test/java/tests/OrderTests.java
@@ -1,3 +1,17 @@
 package tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class OrderTests extends BaseTests{
+
+    @ParameterizedTest()
+    @DisplayName()
+    @CsvSource({
+
+    })
+    public void order_without_account_creation(String productUrl){
+
+    }
 }
