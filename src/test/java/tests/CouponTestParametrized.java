b52282d369b797c4efaf1416180e636157a378b5
rafbac <r.baczynski.sielaczek@gmail.com>
2024-10-30 16:25:28 +0100

    new test CouponTestParametrized: - sing_coupon_kwotowy300 - using_coupon_kwotowy_bez_promocji_positive - using_coupon_kwotowy_bez_promocji_negative - using_coupon_windsurfing350_positive - using_coupon_windsurfing350_negative

--- src/test/java/tests/CouponTestParametrized.java
+++ src/test/java/tests/CouponTestParametrized.java
@@ -20,7 +20,6 @@ import pageobjects.ProductPage;
 
 public class CouponTestParametrized extends BaseTests{
     @ParameterizedTest(name = "Product {0} added to Card with coupon: inactive")
-    @DisplayName("Using coupon - inactive")
     @CsvSource({
             "/fuerteventura-sotavento/",
             "/egipt-el-gouna/",
@@ -52,7 +51,6 @@ public class CouponTestParametrized extends BaseTests{
                 "Coupon message is not what expected");
     }
     @ParameterizedTest(name = "Product {0} added to Card with coupon: not existing")
-    @DisplayName("Using coupon - not existing")
     @CsvSource({
             "/fuerteventura-sotavento/",
             "/egipt-el-gouna/",
@@ -84,7 +82,6 @@ public class CouponTestParametrized extends BaseTests{
                 "Coupon message is not what expected");
     }
     @ParameterizedTest(name = "Product {0} added to Card with coupon: 250nolimit ")
-    @DisplayName("Using coupon - 250 no limit")
     @CsvSource({
             "/fuerteventura-sotavento/",
             "/egipt-el-gouna/",
@@ -116,7 +113,6 @@ public class CouponTestParametrized extends BaseTests{
                 "Coupon massage is not what expected");
     }
     @ParameterizedTest(name = "Product {0} added to Card with coupons: 250nolimit + 10%")
-    @DisplayName("Using coupon - 250nolimit + 10%")
     @CsvSource({
             "/fuerteventura-sotavento/",
             "/egipt-el-gouna/",
@@ -182,6 +178,123 @@ public class CouponTestParametrized extends BaseTests{
         Assertions.assertEquals(cartPage.getSuccessMessage(),
                 "Przepraszamy, kupon \"kwotowy250pojedynczy\" " +
                         "został już użyty nie może być powiązany z innymi kuponami.",
+                "Coupon message is not what expected");
+    }
+    @ParameterizedTest(name= "Product {0} was added to card with coupon: kwotowy300")
+    @CsvSource({
+            "/fuerteventura-sotavento/",
+            "/egipt-el-gouna/",
+            "/grecja-limnos/",
+            "/windsurfing-w-lanzarote-costa-teguise/",
+            "/wyspy-zielonego-przyladka-sal/",
+            "/wspinaczka-island-peak/",
+            "/wakacje-z-yoga-w-kraju-kwitnacej-wisni/",
+            "/wczasy-relaksacyjne-z-yoga-w-toskanii/",
+            "/zmien-swoja-sylwetke-yoga-na-malcie/",
+    })
+    public void using_coupon_kwotowy300(String productUrl){
+        String coupon = "kwotowy300";
+        ProductPage productPage = new ProductPage(browser);
+        CartPage cartPage = productPage
+                .go(productUrl)
+                .closeInfoButton()
+                .addToCart()
+                .goToCart()
+                .applyCoupon(coupon);
+        Assertions.assertEquals(cartPage.getSuccessMessage(),
+                "Kupon został pomyślnie użyty.",
+                "Coupon message is not what expected");
+    }
+    @ParameterizedTest(name = "Product {0} was added to card with coupon: kwotowy300bezpromocji")
+    @CsvSource({
+            "/fuerteventura-sotavento/",
+            "/egipt-el-gouna/",
+            "/grecja-limnos/",
+            "/windsurfing-w-karpathos/",
+            "/windsurfing-w-lanzarote-costa-teguise/",
+            "/wyspy-zielonego-przyladka-sal/",
+            "/gran-koscielcow/",
+            "/wspinaczka-island-peak/",
+            "/wczasy-relaksacyjne-z-yoga-w-toskanii/",
+            "/yoga-i-pilates-w-hiszpanii/",
+            "/zmien-swoja-sylwetke-yoga-na-malcie/",
+    })
+    public void using_coupon_kwotowy_bez_promocji_positive(String productUrl){
+        String coupon = "kwotowy300bezpromocji";
+        ProductPage productPage = new ProductPage(browser);
+        CartPage cartPage = productPage
+                .go(productUrl)
+                .closeInfoButton()
+                .addToCart()
+                .goToCart()
+                .applyCoupon(coupon);
+        Assertions.assertEquals(cartPage.getSuccessMessage(),
+                "Kupon został pomyślnie użyty.",
+                "Coupon message is not what expected");
+    }
+    @ParameterizedTest(name = "Product {0} was added to card with coupon: kwotowy300bezpromocji - negative")
+    @CsvSource({
+            "/wspinaczka-via-ferraty/",
+            "/wakacje-z-yoga-w-kraju-kwitnacej-wisni/",
+            "/yoga-i-pilates-w-portugalii/",
+            "/kurs-zeglarski-na-mazurach/",
+    })
+    public void using_coupon_kwotowy_bez_promocji_negative(String productUrl){
+        String coupon = "kwotowy300bezpromocji";
+        ProductPage productPage = new ProductPage(browser);
+        CartPage cartPage = productPage
+                .go(productUrl)
+                .closeInfoButton()
+                .addToCart()
+                .goToCart()
+                .applyCoupon(coupon);
+        Assertions.assertEquals(cartPage.getSuccessMessage(),
+                "Przepraszamy, ten kupon nie może być zastosowany do przecenionych produktów.",
+                "Coupon message is not what expected");
+    }
+    @ParameterizedTest(name = "Producj {0} was added to card with coupon: windsurfing350")
+    @DisplayName("Using coupon: windsurfing350 - positive")
+    @CsvSource({
+            "/fuerteventura-sotavento/",
+            "/egipt-el-gouna/",
+            "/grecja-limnos/",
+            "/windsurfing-w-karpathos/",
+            "/windsurfing-w-lanzarote-costa-teguise/",
+            "/wyspy-zielonego-przyladka-sal/",
+            "/gran-koscielcow/",
+    })
+    public void using_coupon_windsurfing350_positive(String productUrl){
+        String copupon = "windsurfing350";
+        ProductPage productPage = new ProductPage(browser);
+        CartPage cartPage = productPage
+                .go(productUrl)
+                .closeInfoButton()
+                .addToCart()
+                .goToCart()
+                .applyCoupon(copupon);
+        Assertions.assertEquals(cartPage.getSuccessMessage(),
+                "Kupon został pomyślnie użyty.",
+                "Coupon message is not what expected");
+    }
+    @ParameterizedTest(name = "Product {0} was added tpo cart with coupon: windsurfing350")
+    @DisplayName("Using coupon: windsurfing350 - negative")
+    @CsvSource({
+            "/wspinaczka-island-peak/",
+            "/wczasy-relaksacyjne-z-yoga-w-toskanii/",
+            "/yoga-i-pilates-w-hiszpanii/",
+            "/zmien-swoja-sylwetke-yoga-na-malcie/",
+    })
+    public void using_coupon_windsurfing350_negative(String productUrl){
+        String coupon = "windsurfing350";
+        ProductPage productPage = new ProductPage(browser);
+        CartPage cartPage = productPage
+                .go(productUrl)
+                .closeInfoButton()
+                .addToCart()
+                .goToCart()
+                .applyCoupon(coupon);
+        Assertions.assertEquals(cartPage.getSuccessMessage(),
+                "Przepraszamy, tego kuponu nie można zastosować do wybranych produktów.",
                 "Coupon message is not what expected");
     }
 }
