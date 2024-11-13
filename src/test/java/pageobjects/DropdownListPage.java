c9260a4a6680174ac27275896b097556065876f9
rafbac <r.baczynski.sielaczek@gmail.com>
2024-11-13 23:27:47 +0100

    DropdownListPage_3

--- src/test/java/pageobjects/DropdownListPage.java
+++ src/test/java/pageobjects/DropdownListPage.java
@@ -1,14 +1,13 @@
 package pageobjects;
 
 import helpers.Browser;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.Select;
 
-public class DropdownListPage extends BasePage{
-    //public static WebElement selectSingleChoiceList;
-    WebElement selectSingleChoiceList = driver.findElement(By.cssSelector("#flavors"));
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class DropdownListPage extends BasePage {
     public DropdownListPage(Browser browser) {
         super(browser);
     }
@@ -16,14 +15,37 @@ public class DropdownListPage extends BasePage{
         driver.get(baseURL + "/lista-rozwijana/");
     }
 
-    public void selectSingleChoice(String value) {
-        Select select = new Select(selectSingleChoiceList);
-        select.selectByValue(value);
+    public final By dropdownSingle = By.id("flavors");
+    public final By dropdownMulti = By.id("flavors-multiple-selected");
+    public void selectSingleChoice(String option) {
+        WebElement dropdownElement = driver.findElement(dropdownSingle);
+        Select select = new Select(dropdownElement);
+        select.selectByVisibleText(option);
     }
 
     public String getSelectedOption() {
-        Select select = new Select(selectSingleChoiceList);
-        return select.getFirstSelectedOption().getAttribute("value");
+        WebElement dropdownElement = driver.findElement(dropdownSingle);
+        Select select = new Select(dropdownElement);
+        return select.getFirstSelectedOption().getText();
     }
 
+
+    public void selectMultipleChoice(String... options) {
+        WebElement dropdownElement = driver.findElement(dropdownMulti);
+        Select select = new Select(dropdownElement);
+        for (String option : options) {
+            select.selectByVisibleText(option);
+        }
+    }
+
+    public List<String> getSelectedOptions() {
+        WebElement dropdownElement = driver.findElement(dropdownMulti);
+        Select select = new Select(dropdownElement);
+        return select.getAllSelectedOptions().stream()
+                .map(WebElement::getText)
+                .collect(Collectors.toList());
+    }
+    public WebElement getDropdownMulti() {
+        return driver.findElement(dropdownMulti);
+    }
 }
